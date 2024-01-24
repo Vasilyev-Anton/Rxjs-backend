5ad33889edd2bf10336230305fa83f6feb8b07e2
Vasilyev-Anton <vasilyev1812@gmail.com>
2024-01-24 20:41:35 +0300

    upd v2.0

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
 // server.js
 const express = require('express');
 const cors = require('cors');
-// eslint-disable-next-line import/no-extraneous-dependencies
 const { faker } = require('@faker-js/faker');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 3000;
 
 app.use(express.json());
-app.use(cors({ origin: '*' }));
+app.use(cors());
 
 app.get('/messages/unread', (req, res) => {
   const generateFakeMessage = () => ({
@@ -20,10 +19,9 @@ app.get('/messages/unread', (req, res) => {
   });
 
   const fakeMessages = Array.from({ length: 1 }, generateFakeMessage);
+  console.log(fakeMessages)
   const response = { status: 'ok', messages: fakeMessages };
   res.json(response);
 });
 
-app.listen(port, () => {
-  console.log(port);
-});
+app.listen(port, '0.0.0.0');
